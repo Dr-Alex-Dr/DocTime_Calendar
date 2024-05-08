82f412819cb4b6cb9b59099e1d846b9ca9162bd3
Алексей Линьков <60229705+Dr-Alex-Dr@users.noreply.github.com>
2024-05-08 19:52:05 +0500

    Замена url на правильный

--- src/entities/table/api/api.ts
+++ src/entities/table/api/api.ts
@@ -1,8 +1,9 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 import { IDoctor } from '../model';
 
-const BASE_URL = "http://localhost:8010/proxy/api/v1/";
-  
+// const BASE_URL = "http://localhost:8010/proxy/api/v1/";
+const BASE_URL = "https://redrock.mycum.ru/api/v1";
+
 export const api = createApi({
     reducerPath: 'api',
     tagTypes: ['WorkTime'],
