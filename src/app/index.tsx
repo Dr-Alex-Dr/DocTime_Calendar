fb3fcf18ea2f3341d717274881f977c3bc3fad9c
Алексей Линьков <60229705+Dr-Alex-Dr@users.noreply.github.com>
2024-06-24 10:39:11 +0500

    auth form

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -3,13 +3,14 @@ import { Calendar } from '../pages/Calendar';
 import { Provider } from "react-redux";
 import { store } from './store';
 import { Login } from '../pages/Login/login';
+import { Registration } from '../pages/Registration/registration';
 
 // потом заменить на нормальный Provider
 const App = () => {
     return (
       <Provider store={store}>
         <div>
-          <Login />  
+          <Registration />  
         </div>
       </Provider>
     )
--- src/pages/Registration/registration.tsx
+++ src/pages/Registration/registration.tsx
@@ -14,105 +14,140 @@ import * as yup from 'yup';
 import styles from './registration.module.scss';
 
 const schema = yup.object().shape({
-  email: yup.string().email('Некорректный email').required('Email обязателен'),
-  password: yup.string()
-    .min(8, 'Ваш пароль должен содержать не менее 8 символов.')  
-    .matches(/^(?!.*(\w)\1{2,}).*$/, 'Ваш пароль слишком простой') 
-    .matches(/^(?![0-9]*$)/, 'Ваш пароль не может быть быть полностью числовым') 
-    .required('Пароль обязателен'),
+    username: yup.string()
+        .max(225, 'Ваш пароль должен быть короче 225 символов')
+        .required('Никнейм обязателен'),
+    email: yup.string().email('Некорректный email').required('Email обязателен'),
+    password: yup.string()
+        .min(8, 'Ваш пароль должен содержать не менее 8 символов.')
+        .max(225, 'Ваш пароль должен быть короче 225 символов')
+        .matches(/^(?!.*(\w)\1{2,}).*$/, 'Ваш пароль слишком простой')
+        .matches(/^(?![0-9]*$)/, 'Ваш пароль не может быть полностью числовым')
+        .required('Пароль обязателен'),
 });
 
+
 export const Registration = () => {
-  const { control, handleSubmit, formState: { errors }, } = useForm(
-    {
+    const { control, handleSubmit, formState: { errors } } = useForm({
         resolver: yupResolver(schema),
         defaultValues: {
+            username: '',
             email: '',
             password: '',
         },
-    }
-);
+    });
 
-  const onSubmit = (data: any) => {
-    console.log(data);
-  };
+    const onSubmit = (data: any) => {
+        console.log(data);
+    };
 
-  return (
-    <div>
-      <Container component="main" maxWidth="xs">
-        <Box     
-          sx={{
-            marginTop: 13,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-          }}
-        >
-          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-            <LockOutlinedIcon />
-          </Avatar>
-          <Typography component="h1" variant="h5">
-            Вход
-          </Typography>
+    return (
+        <Container component="main" maxWidth="xs">
+            <Box sx={sxStyles.container}>
+                <Avatar sx={sxStyles.avatar}>
+                    <LockOutlinedIcon />
+                </Avatar>
+                <Typography component="h1" variant="h5">
+                    Регистрация
+                </Typography>
 
-          <Box component="form" onSubmit={handleSubmit(onSubmit)} noValidate sx={{ mt: 1 }}>
-            <Controller
-              name="email"
-              control={control}
-              render={({ field }) => (
-                <TextField
-                  {...field}
-                  margin="normal"
-                  required
-                  fullWidth
-                  id="email"
-                  label="Email Address"
-                  autoComplete="email"
-                  autoFocus
-                  error={!!errors.email}
-                 
-                />
-              )}
-            />
-            <Controller
-              name="password"
-              control={control}
-              render={({ field }) => (
-                <TextField
-                  {...field}
-                  margin="normal"
-                  required
-                  fullWidth
-                  name="password"
-                  label="Password"
-                  type="password"
-                  id="password"
-                  autoComplete="current-password"
-                  error={!!errors.password}
-                  
-                />
-              )}
-            />
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
-            >
-              Войти
-            </Button>
-            <Grid container>
-              <Grid item xs>
-              </Grid>
-              <Grid item>
-                <Link href="#" variant="body2">
-                  {"Зарегестрироваться "}
-                </Link>
-              </Grid>
-            </Grid>
-          </Box>
-        </Box>
-      </Container>
-    </div>
-  );
+                <Box component="form" onSubmit={handleSubmit(onSubmit)} noValidate sx={sxStyles.form}>
+                    <Controller
+                        name="username"
+                        control={control}
+                        render={({ field }) => (
+                            <TextField
+                                {...field}
+                                margin="normal"
+                                required
+                                fullWidth
+                                id="username"
+                                label="Nickname"
+                                autoComplete="Nickname"
+                                autoFocus
+                                error={!!errors.username}
+                                helperText={errors?.username?.message as string}
+                            />
+                        )}
+                    />
+                    <Controller
+                        name="email"
+                        control={control}
+                        render={({ field }) => (
+                            <TextField
+                                {...field}
+                                margin="normal"
+                                required
+                                fullWidth
+                                id="email"
+                                label="Email Address"
+                                autoComplete="email"
+                                autoFocus
+                                error={!!errors.email}
+                                helperText={errors?.email?.message as string}
+                            />
+                        )}
+                    />
+                    <Controller
+                        name="password"
+                        control={control}
+                        render={({ field }) => (
+                            <TextField
+                                {...field}
+                                margin="normal"
+                                required
+                                fullWidth
+                                name="password"
+                                label="Password"
+                                type="password"
+                                id="password"
+                                autoComplete="current-password"
+                                error={!!errors.password}
+                                helperText={errors?.password?.message as string}
+                            />
+                        )}
+                    />
+                    <Button
+                        type="submit"
+                        fullWidth
+                        variant="contained"
+                        sx={sxStyles.submitButton}
+                    >
+                        Войти
+                    </Button>
+                    <Grid container>
+                        <Grid item xs>
+                        </Grid>
+                        <Grid item>
+                            <Link href="#" variant="body2">
+                                {"Зарегестрироваться "}
+                            </Link>
+                        </Grid>
+                    </Grid>
+                </Box>
+            </Box>
+        </Container>
+    )
+};
+
+
+
+const sxStyles = {
+    container: {
+        marginTop: 13,
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+    },
+    avatar: {
+        m: 1,
+        bgcolor: 'secondary.main',
+    },
+    form: {
+        mt: 1,
+    },
+    submitButton: {
+        mt: 3,
+        mb: 2,
+    },
 };
