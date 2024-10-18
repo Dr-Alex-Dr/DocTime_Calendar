2199d1cb60d47b28a4bef3767229037285da7309
Delta <mr.lopin900@gmail.com>
2024-10-18 17:34:58 +0500

    Добавлены файлы конфигурации

--- src/entities/table/lib/cabinetsSlice.ts
+++ src/entities/table/lib/cabinetsSlice.ts
@@ -1,41 +1,41 @@
-import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
-import { ICabinet } from '../model';
-import axios from 'axios';
-import {baseUrl} from "../../../shared/const/url";
+// redux/cabinetsSlice.js
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { fetchBaseQuery } from "@reduxjs/toolkit/query";
+import { ICabinet } from "../model";
+import axios from "axios";
+import { baseUrl } from "../../../shared/const/url";
 
-export const getCabinets = createAsyncThunk<ICabinet[]>('cabinets/getCabinets', async () => {
-    const response = await axios.get(`${baseUrl}/cabinets`, {
-      headers: {
-         'Authorization': `Bearer ${localStorage.getItem('token')}`
-      }
-    })
+export const getCabinets = createAsyncThunk<ICabinet[]>(
+  "cabinets/getCabinets",
+  async () => {
+    const response = await axios.get(`${baseUrl}/cabinets`);
 
-    return response.data
-});
+    return response?.data;
+  }
+);
 
 const cabinetsSlice = createSlice({
-  name: 'cabinets',
+  name: "cabinets",
   initialState: {
     items: [] as ICabinet[],
-    status: 'idle',
+    status: "idle",
     error: null as string | null,
   },
   reducers: {},
-  extraReducers: builder => {
+  extraReducers: (builder) => {
     builder
-        .addCase(getCabinets.pending, state => {
-          state.status = 'loading';
-        })
-        .addCase(getCabinets.fulfilled, (state, action) => {
-          state.status = 'succeeded';
-          state.items = action.payload;
-        })
-        .addCase(getCabinets.rejected, (state, action) => {
-          state.status = 'failed';
-          state.error = action.error.message || 'Failed to fetch cabinets';
-        });
+      .addCase(getCabinets.pending, (state) => {
+        state.status = "loading";
+      })
+      .addCase(getCabinets.fulfilled, (state, action) => {
+        state.status = "succeeded";
+        state.items = action.payload;
+      })
+      .addCase(getCabinets.rejected, (state, action) => {
+        state.status = "failed";
+        state.error = action.error.message || "Failed to fetch cabinets";
+      });
   },
 });
 
 export default cabinetsSlice.reducer;
-
